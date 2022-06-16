50b4fc57d74bcbc994c733530e677272e7a69ea3
Nzovia <nickiemaundu19@gmail.com>
2022-06-16 17:11:32 +0300

    initial commit

---
+++ src/main/java/com/example/kejani_backend/KejaniBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.kejani_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KejaniBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KejaniBackendApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/kejani_backend/Usermanagement/entities/HouseOwner.java
@@ -0,0 +1,4 @@
+package com.example.kejani_backend.Usermanagement.entities;
+
+public class HouseOwner {
+}
